004b97ca1d09323836c9e31b72d0df51618144cd
Abhishek Vats <virus@luffy-book.local>
2024-02-17 02:00:04 +0530

    chore: pdf scraping and excel sheet creating is complete

--- College-result-pdf-scraping/main.py
+++ College-result-pdf-scraping/main.py
@@ -1,6 +1,7 @@
 import re
 import pandas as pd
 import PyPDF2
+from io import StringIO
 
 
 def get_ese_marks(singlePage, sem: int, sub: int):
@@ -181,7 +182,7 @@ def sem3Data(singlePage):
             "Total": sub6_total_marks,
             "Sts": sub6_sts_value,
         },
-        "Total Credit": total_credit,
+        "Total_Credit": total_credit,
         "EGP": egp,
         "SGPA": sgpa,
     }
@@ -324,7 +325,7 @@ def sem4Data(singlePage):
             "ISE": subenv_ise_marks,
             "Total": subenv_total_marks,
         },
-        "Total Credit": total_credit,
+        "Total_Credit": total_credit,
         "EGP": egp,
         "SGPA": sgpa,
     }
@@ -372,8 +373,8 @@ def singlePageData(singlePage):
         percentage = ""
 
     return {
-        "Exam Seat No.": seat_no,
-        "PRN No.": prn_no,
+        "Exam_Seat_No": seat_no,
+        "PRN_No": prn_no,
         "Name": name,
         "Sem3": sem3_data,
         "Sem4": sem4_data,
@@ -397,12 +398,139 @@ def extract_data_from_pdf(pdf_path):
 pdf_path = "input.pdf"
 data_list = extract_data_from_pdf(pdf_path)
 
-pd.set_option("display.max_columns", None)
-pd.set_option("display.expand_frame_repr", False)
+html_head = """
+<thead>
+    <tr>
+        <td rowspan="3">Exam Seat No.</td>
+        <td rowspan="3">PRN No.</td>
+        <td rowspan="3">Name of Student</td>
+        <td colspan="39">Sem 3</td>
+        <td colspan="42">Sem 4</td>
+        <td rowspan="3">Status</td>
+        <td rowspan="3">Percentage</td>
+    </tr>
+    <tr>
+        <td colspan="6">BTN06301</td>
+        <td colspan="6">BTN06302</td>
+        <td colspan="6">BTN06303</td>
+        <td colspan="6">BTN06304</td>
+        <td colspan="6">BTN06305</td>
+        <td colspan="6">BTN06306</td>
+        <td rowspan="2">Total Credit</td>
+        <td rowspan="2">EGP</td>
+        <td rowspan="2">SGPA</td>
+        <td colspan="6">BTN06401</td>
+        <td colspan="6">BTN06402</td>
+        <td colspan="6">BTN06403</td>
+        <td colspan="6">BTN06404</td>
+        <td colspan="6">BTN06405</td>
+        <td colspan="6">BTN06406</td>
+        <td colspan="3">BTNENV</td>
+        <td rowspan="2">Total Credit</td>
+        <td rowspan="2">EGP</td>
+        <td rowspan="2">SGPA</td>
+    </tr>
+    <tr>
+        <td>ESE</td>
+        <td>ISE</td>
+        <td>ICA</td>
+        <td>POE</td>
+        <td>Total</td>
+        <td>Sts</td>
+        <td>ESE</td>
+        <td>ISE</td>
+        <td>ICA</td>
+        <td>POE</td>
+        <td>Total</td>
+        <td>Sts</td>
+        <td>ESE</td>
+        <td>ISE</td>
+        <td>ICA</td>
+        <td>POE</td>
+        <td>Total</td>
+        <td>Sts</td>
+        <td>ESE</td>
+        <td>ISE</td>
+        <td>ICA</td>
+        <td>POE</td>
+        <td>Total</td>
+        <td>Sts</td>
+        <td>ESE</td>
+        <td>ISE</td>
+        <td>ICA</td>
+        <td>POE</td>
+        <td>Total</td>
+        <td>Sts</td>
+        <td>ESE</td>
+        <td>ISE</td>
+        <td>ICA</td>
+        <td>POE</td>
+        <td>Total</td>
+        <td>Sts</td>
+        <td>ESE</td>
+        <td>ISE</td>
+        <td>ICA</td>
+        <td>POE</td>
+        <td>Total</td>
+        <td>Sts</td>
+        <td>ESE</td>
+        <td>ISE</td>
+        <td>ICA</td>
+        <td>POE</td>
+        <td>Total</td>
+        <td>Sts</td>
+        <td>ESE</td>
+        <td>ISE</td>
+        <td>ICA</td>
+        <td>POE</td>
+        <td>Total</td>
+        <td>Sts</td>
+        <td>ESE</td>
+        <td>ISE</td>
+        <td>ICA</td>
+        <td>POE</td>
+        <td>Total</td>
+        <td>Sts</td>
+        <td>ESE</td>
+        <td>ISE</td>
+        <td>ICA</td>
+        <td>POE</td>
+        <td>Total</td>
+        <td>Sts</td>
+        <td>ESE</td>
+        <td>ISE</td>
+        <td>ICA</td>
+        <td>POE</td>
+        <td>Total</td>
+        <td>Sts</td>
+        <td>ESE</td>
+        <td>ISE</td>
+        <td>Total</td>
+    </tr>
+</thead>
+"""
 
-df = pd.DataFrame(data_list)
-# print(df)
+all_data = ""
+for student in data_list:
+    one_row = ""
+    one_row += f"<td>{student['Exam_Seat_No']}</td>"
+    one_row += f"<td>{student['PRN_No']}</td>"
+    one_row += f"<td>{student['Name']}</td>"
+
+    for i in ["Sem3", "Sem4"]:
+        for data in student[i]:
+            # Check if the value is a dictionary before iterating over it
+            if isinstance(student[i][data], dict):
+                for abb in student[i][data]:
+                    one_row += f"<td>{student[i][data][abb]}</td>"
+            else:
+                one_row += f"<td>{student[i][data]}</td>"
+
+    one_row += f"<td>{student['Status']}</td>"
+    one_row += f"<td>{student['Percentage']}</td>"
+    all_data += f"<tr>{one_row}</tr>"
 
 
-output_excel_path = "output.xlsx"
-df.to_excel(output_excel_path, index=False)
+table = f"<table>{html_head}<tbody>{all_data}</tbody></table>"
+result = pd.read_html(StringIO(table))
+result[0].to_excel("output.xlsx")
